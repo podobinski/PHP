eaa13b4195d187a675dcad68157bd12720683170
podobinski <piotr.podobinski@gmail.com>
2024-01-22 21:13:53 +0100

    01_07

---
+++ 01_07.php
@@ -0,0 +1,9 @@
+<?php 
+$name= "Teresa"; // Heading 1
+$city = "NYC";
+$movie = "Star Wars"; //Italics
+$friend = "George"; //Bold
+$candy = "Sour Patch Kids";
+
+echo "Dawno temu pewna <h1> $name </h1> zabrała do $city kolegę <b>$friend</b> na film <i>$movie</i>. <br>
+Zajadali sie przy tym $candy";
